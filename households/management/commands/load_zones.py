58e2fa948f4f09599d5ba804d2989e40e4a64b63
anj-zoldyck <dizonjelo50@gmail.com>
2026-07-05 15:52:03 +0800

    updated zones

--- households/management/commands/load_zones.py
+++ households/management/commands/load_zones.py
@@ -10,15 +10,28 @@ class Command(BaseCommand):
             "Becuran": 6,
             "Dila-Dila": 7,
             "San Matias": 4,
-            "San Vicente": 5,
+            "San Vicente": 4,
             "Santa Monica": 4,
-            "San Agustin": 3,
-            "San Jose": 5,
+            "San Agustin": 4,
+            "San Jose (Poblacion)": 7,
             "San Juan Macaba": 3,
             "San Juan Cacutud": 3,
             "San Isidro": 4,
             "San Basilio": 8
         }
+
+        # Handle San Jose rename if it exists under old name
+        try:
+            old_san_jose = Barangay.objects.get(name="San Jose")
+            self.stdout.write(
+                self.style.WARNING(
+                    f'Found existing "San Jose" barangay. Renaming to "San Jose (Poblacion)"'
+                )
+            )
+            old_san_jose.name = "San Jose (Poblacion)"
+            old_san_jose.save()
+        except Barangay.DoesNotExist:
+            pass
 
         for barangay_name, count in zones_data.items():
             barangay, _ = Barangay.objects.get_or_create(name=barangay_name)
